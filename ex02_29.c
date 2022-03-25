3a5adf4c571ac9ad96f9f3e931b68452b2860433
Pavlov Vladimir <pav91pav@mail.ru>
2022-03-25 14:10:25 -0700

    make ex02_29.c

---
+++ ex02_29.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("A=%d\nB=%d\nC=%d\na=%d\nb=%d\nc=%d\n0=%d\n1=%d\n2=%d\n$=%d\n*=%d\n+=%d\n/=%d\nspace=%d\n",'A','B','C','a','b','c','0','1','2','$','*','+','/',' ');
+	return 0;
+}
