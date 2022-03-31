094575dad09bc486f770726ae6eb33ac7d0acde2
Pavlov Vladimir <pav91pav@mail.ru>
2022-03-31 11:44:42 +0300

    ex03_39.c

--- ex03_39.c
+++ ex03_39.c
@@ -4,6 +4,7 @@ int main()
 {
 	int number;
 	int num1,num2,num3,num4,num5;
+	int counter=0;
 	fflush(stdout);
 	setbuf(stdout,NULL);
 	printf("Please,enter number from 5 element\n");
@@ -13,6 +14,17 @@ int main()
 	num3=(number-num1*10000-num2*1000)/100;
 	num4=(number-num1*10000-num2*1000-num3*100)/10;
 	num5=(number-num1*10000-num2*1000-num3*100-num4*10);
-	printf("%d %d %d %d %d",num1,num2,num3,num4,num5);
+	printf("%d %d %d %d %d\n",num1,num2,num3,num4,num5);
+	if(num1==7)
+		counter++;
+	if(num2==7)
+		counter++;
+	if(num3==7)
+		counter++;
+	if(num4==7)
+		counter++;
+	if(num5==7)
+		counter++;
+	printf("number 7 in enter number = %d\n",counter);
 	return 0;
 }
