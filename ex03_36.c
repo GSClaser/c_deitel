42b2a95a49e02337380c7faf5fa8cd9fd10b9dc7
Pavlov Vladimir <pav91pav@mail.ru>
2022-03-31 09:09:39 +0300

    add example ex03_35.c and ex03_36.c

---
+++ ex03_36.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int number;
+	int num1,num2,num3,num4,num5;
+	fflush(stdout);
+	setbuf(stdout,NULL);
+	printf("Please,enter number from 5 element\n");
+	scanf("%d",&number);
+	num1=number/10000;
+	num2=(number-num1*10000)/1000;
+	num3=(number-num1*10000-num2*1000)/100;
+	num4=(number-num1*10000-num2*1000-num3*100)/10;
+	num5=(number-num1*10000-num2*1000-num3*100-num4*10);
+	printf("%d %d %d %d %d\n",num1,num2,num3,num4,num5);
+	printf("%d\n",(int)(num1*pow(2,4)+num2*pow(2,3)+num3*pow(2,2)+num4*pow(2,1)+num5*pow(2,0)));
+	return 0;
+}
