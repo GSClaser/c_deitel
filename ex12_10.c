adb78aefd8dd67770fed22c0178c490380645486
Pavlov Vladimir <pav91pav@mail.ru>
2022-07-21 20:47:36 +0300

    make example ex12_10.c about stack

---
+++ ex12_10.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+struct Stack
+{
+	char value;
+	struct Stack* next;
+};
+
+void addStack(struct Stack** list,char value);
+int isEmpty(struct Stack* stack);
+void printStack(struct Stack* stack);
+
+int main()
+{
+	char ch[50];
+	int i=0;
+	struct Stack* stack=NULL;
+	printf("enter text: ");
+	scanf("%s",ch);
+	while(ch[i])
+	{
+		addStack(&stack,ch[i]);
+		i++;
+	}
+	while(!isEmpty(stack))
+	{
+		printStack(&stack);
+	}
+}
+
+void addStack(struct Stack** stack,char value)
+{
+	struct Stack* newStack = malloc(sizeof(struct Stack));
+	if(newStack!=NULL)
+	{
+		newStack->value=value;
+		newStack->next=*stack;
+		*stack=newStack;
+	}
+	else
+	{
+		puts("can't allocate memory");
+	}
+}
+
+int isEmpty(struct Stack* stack)
+{
+	return stack==NULL;
+}
+
+void printStack(struct Stack** stack)
+{
+	struct Stack* temp=*stack;
+	printf("%c",(*stack)->value);
+	*stack=(*stack)->next;
+	free(temp);
+}
